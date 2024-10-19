19698da3a84fa56a5ed5ef2b25fe6f89e4b673ab
Husni <husniwfayo@gmail.com>
2024-10-19 23:16:29 +0530

    add my name to contribute, thanks for opportunity my first open source contribution

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,9 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
-  
+  {
+    id: 428,
+    fullname: "Husni Haniffa",
+    username: "https://github.com/Husnixix"
+  }
 ];
